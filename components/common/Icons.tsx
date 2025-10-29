c3e44cd91984d939f07bb635d50f92e453c35dd6
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-10-29 23:04:50 +0700

    Add UsersIcon component to Icons.tsx

--- components/common/Icons.tsx
+++ components/common/Icons.tsx
@@ -104,3 +104,9 @@ export const TagIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
         <path strokeLinecap="round" strokeLinejoin="round" d="M6 6h.008v.008H6V6z" />
     </svg>
 );
+
+export const UsersIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-4.663M15 19.128a9.37 9.37 0 00-7.16-3.228 9.37 9.37 0 00-1.833.229M10.5 16.5c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3z" />
+    </svg>
+);
