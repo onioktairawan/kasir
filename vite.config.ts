7797abbce2a4c62ea9a0bcc5bfb11f312429c307
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-10-30 00:57:20 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // String shorthand: http://localhost:5173/api/users -> http://localhost:3001/api/users
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    }
+  }
 })
